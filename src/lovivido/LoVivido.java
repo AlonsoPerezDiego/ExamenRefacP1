4ffd87c0364868a4dea01f05a32e2414d2bde3e9
Diego <Diego@Diego>
2016-12-12 09:26:47 +0100

    Eliminadas clases que daban errores arreglandolos en el método mostrar(), reducido el código  y creado objeto que llama al método mostrar().

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -9,6 +9,8 @@ public class LoVivido {
 
 public static void main(String[] args) { 
 
+    Metodos calculoEdad = new Metodos();//Objeto para la clase Metodos.
+    calculoEdad.mostrar();//Se llama al método mostrar().
 }
    
 
--- src/lovivido/Metodos.java
+++ src/lovivido/Metodos.java
@@ -26,37 +26,13 @@ public class Metodos {
         return edad;
     }
     
-    private int añosDeVida(){
-        //Método para pasar el String edad a un int.
-        int años = Integer.parseInt(edad());
-        return años;
-    }
     
-    private int meses(){
-        //Método para calcular los meses en funcion de la edad introducida.
-        int meses = añosDeVida()*12;
-        return meses;
-    }
-    
-    private int dias(){
-        //Método para calcular los días en funcion de la edad introducida.
-        int dias = añosDeVida()*365;
-        return dias;
-    }
-    
-    private int horas(){
-        //Método para calcular las horas en funcion de la edad introducida.
-        int horas = dias()*24;
-        return horas;
-    }
-    
-    public void mostrar(){
-        //Método para mostrar todo lo calculado hasta ahora.
-        JOptionPane.showMessageDialog(null,  "Meses: "  +meses(),"Numero de meses vividos de " +nombre(),JOptionPane.INFORMATION_MESSAGE);
-
-        JOptionPane.showMessageDialog(null, "Días: " +dias(),"Numero de días vividos de "+nombre(),JOptionPane.INFORMATION_MESSAGE);
-
-        JOptionPane.showMessageDialog(null, "Horas: " +horas(),"Numero de horas vividos de "+nombre(),JOptionPane.INFORMATION_MESSAGE);
+    public void mostrar(){ //Método para mostrar todo lo calculado hasta ahora.
+        String name = nombre();
+        int años =  Integer.parseInt(edad());
+        JOptionPane.showMessageDialog(null,  "Meses: "  +años*12,"Numero de meses vividos de " +name,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Días: " +años*365,"Numero de días vividos de "+name,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Horas: " +años*365*24,"Numero de horas vividos de "+name,JOptionPane.INFORMATION_MESSAGE);
 
         System.exit (0);
     }
