6b88807994c6e39102ff103f2728ba6042a4bcb9
Diego <Diego@Diego>
2016-12-12 09:17:47 +0100

    Método para mostrar todo lo calculado hasta ahora.

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -4,21 +4,10 @@
  */
 package lovivido;
 
-import javax.swing.JOptionPane;
 
 public class LoVivido {
 
 public static void main(String[] args) { 
- 
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-
-   System.exit (0);
 
 }
    
--- src/lovivido/Metodos.java
+++ src/lovivido/Metodos.java
@@ -49,4 +49,15 @@ public class Metodos {
         int horas = dias()*24;
         return horas;
     }
+    
+    public void mostrar(){
+        //Método para mostrar todo lo calculado hasta ahora.
+        JOptionPane.showMessageDialog(null,  "Meses: "  +meses(),"Numero de meses vividos de " +nombre(),JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Días: " +dias(),"Numero de días vividos de "+nombre(),JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Horas: " +horas(),"Numero de horas vividos de "+nombre(),JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit (0);
+    }
 }
